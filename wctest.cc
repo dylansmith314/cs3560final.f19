5173cba5e6b5e6e95ce19992f53dcd6c1c0cfb00
Dylan <dylansmith314@gmail.com>
2019-12-10 17:35:50 -0500

    Adding test answers.doc

--- wctest.cc
+++ wctest.cc
@@ -19,25 +19,25 @@ int countLine(std::string str2){
     return count;
 }
 
-//Testing pass for name
+//Testing pass for characters in a name
 TEST_CASE("Verifying # of Characters is right using countChar") {
-    std::string s1 = "Ohio";
-  REQUIRE(countChar(s1) == 4);
+    std::string s1 = "Dylan";
+  REQUIRE(countChar(s1) == 5);
 }
 
-//Testing pass for empty string
+//Testing pass for characters in an empty string
 TEST_CASE("Testing countChar to return the right output"){
     std::string s1 = "";
   REQUIRE(countChar(s1) == 0);
 }
 
-//Testing pass for name
+//Testing pass for lines in a name
 TEST_CASE("Verifying # of Lines is right using countLine") {
     std::string s1 = "Ohio";
   REQUIRE(countLine(s1) == 1);
 }
 
-//Testing pass for empty string
+//Testing pass for lines in an empty string
 TEST_CASE("Testing countLine to return the right output"){
     std::string s1 = "";
   REQUIRE(countLine(s1) == 0);
